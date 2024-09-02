87cbaab7ffd5fe22d27a911e9ed213d7f1ec45a6
ganeshkumarrajan <ganeshapikey@gmail.com>
2024-09-02 21:01:28 +0200

    remove unused comments

--- common/src/main/java/com/example/common/Result.kt
+++ common/src/main/java/com/example/common/Result.kt
@@ -2,7 +2,6 @@ package com.example.common
 
 
 sealed class Result<out R> {
-   // data object Loading : Result<Nothing>()
     data class Success<out T>(val data: T) : Result<T>()
     data class Error(val exception: Exception) : Result<Nothing>()
 }
